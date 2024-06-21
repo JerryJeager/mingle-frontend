fe6329250027f4b86c5cf7e17b8ddb2805896448
Amadi Jerry <amadijerry310@gmail.com>
2024-06-21 18:04:18 +0100

    initial repo setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,20 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        "yellow": "#ffcb45",
+        "dark1": "#1e1e1e",
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "dark-grid": "url(../public/images/darkgrid.jpg)",
+        "dark-grid2": "url(../public/images/darkgrid.jpg)",
+        "dark-grid3": "url(../public/images/bg1.jpg)",
+        "dark-grid4": "url(../public/images/bg2.jpg)",
+        "dark-grid5": "url(../public/images/bg3.jpg)",
+        "dark-grid6": "url(../public/images/bg4.jpg)",
       },
     },
   },
